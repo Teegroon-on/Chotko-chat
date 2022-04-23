d75b9200179af363197d61296275fe3c8966d4df
Teegroon <teegroon@gmail.com>
2022-04-23 13:23:21 +0300

    Clear project. Add tailwind.

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-stretch h-screen h-screen overflow-hidden">
+    <main class="w-full h-screen">
+      <div class="w-full h-full">
+        <Nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <div class="h-full">
+
+  </div>
 </template>
 
 <script>
@@ -7,3 +9,5 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style src="../assets/css/main.css"></style>

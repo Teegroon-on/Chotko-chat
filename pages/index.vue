5391e253096e98cdfa69430c11f13ceb97e342cd
Teegroon <teegroon@gmail.com>
2022-04-27 11:34:33 +0300

    Start page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="h-full">
-
+    <div class="h-full flex flex-col items-center justify-center">
+        <img class="w-auto mx-auto" src="../assets/image/logo.jpg" alt="logo">
+        <h2 class="h-1 font-bold text-center text-2xl">Чат</h2>
+        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96 mt-10">Авторизоваться</button>
+        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96 my-2">Зарегистрироваться</button>
+        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96">Войти как анонимный пользователь</button>
+    </div>
   </div>
 </template>
 

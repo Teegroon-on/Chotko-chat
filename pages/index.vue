b5d79de9de188b4bffbfdbd74f35115d8139c4af
Teegroon <teegroon@gmail.com>
2022-04-27 12:51:09 +0300

    Added registration page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="h-full">
     <div class="h-full flex flex-col items-center justify-center">
-        <img class="w-auto mx-auto" src="../assets/image/logo.jpg" alt="logo">
-        <h2 class="h-1 font-bold text-center text-2xl">Чат</h2>
-        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96 mt-10">Авторизоваться</button>
+      <img class="w-auto mx-auto" src="../assets/image/logo.jpg" alt="logo">
+      <h2 class="h-1 font-bold text-center text-2xl">Чат</h2>
+      <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96 mt-10">Авторизоваться</button>
+      <nuxt-link to="/registration">
         <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96 my-2">Зарегистрироваться</button>
-        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96">Войти как анонимный пользователь</button>
+      </nuxt-link>
+      <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-96">Войти как анонимный пользователь</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  methods:{
+
+  }
 }
 </script>
 
---
+++ pages/registration.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-full w-full flex flex-col">
+    <aside class="bg-blue-600 w-full h-20">
+      <img class="w-auto h-20" src="../assets/image/logo.jpg" alt="logo">
+    </aside>
+    <div class="min-h-screen w-full flex flex-col items-center justify-center">
+     <div class="grid grid-rows-6 grid-cols-2 gap-2">
+       <label class="mx-2 mt-0.5 text-xl">Введите Никнейм:</label>
+       <input class="block px-1.5 py-1.5 bg-blue-300 border-blue-700 text-black rounded" type="text">
+       <label class="mx-2 mt-0.5 text-xl">Введите Логин:</label>
+       <input class="block px-1.5 py-1.5 bg-blue-300 border-blue-700 text-black rounded" type="text">
+       <label class="mx-2 mt-0.5 text-xl">Введите email:</label>
+       <input class="block px-1.5 py-1.5 bg-blue-300 border-blue-700 text-black rounded" type="text">
+       <label class="mx-2 mt-0.5 text-xl">Введите пароль:</label>
+       <input class="block px-1.5 py-1.5 bg-blue-300 border-blue-700 text-black rounded" type="text">
+       <label class="mx-2 mt-0.5 text-xl">Повторите пароль:</label>
+       <input class="block px-1.5 py-1.5 bg-blue-300 border-blue-700 text-black rounded" type="text">
+      </div>
+      <div class="flex">
+        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-50 mr-2">Зарегистрироваться</button>
+        <button class="inline-block text-white bg-blue-600 font-medium rounded uppercase px-6 py-2.5 w-50">Войти анонимно</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "registration"
+}
+</script>
+
+<style src="../assets/css/main.css">
+
+</style>
